0fe21b8bd8799e90848b4db7740f1e50657839ac
markus <baumann@cosmocode.de>
2013-03-11 23:14:15 +0100

    initial commit

---
+++ ext_tables.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'Simpleform',
+	'Simple Form'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Simple Form');
+
+?>
